c951bf490569011c3ae5cc00ca2e5016adbd5ae6
zacblazic@gmail.com <zacblazic@gmail.com@9985715c-20a8-dc4d-9a6d-a29f60bc5474>
2012-02-10 20:07:47 +0000

    Added the account package containing a simple account service.

---
+++ Assignment1/test/za/ac/cput/account/test/AccountJUnitTest.java
@@ -0,0 +1,59 @@
+
+package za.ac.cput.account.test;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.junit.*;
+import za.ac.cput.account.Account;
+import za.ac.cput.account.conf.AccountModule;
+
+/**
+ *
+ * @author Zac Blazic
+ */
+public class AccountJUnitTest {
+    
+    private static Injector injector;
+    private static Account account;
+    
+    public AccountJUnitTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception 
+    {
+        injector = Guice.createInjector(new AccountModule());
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() 
+    {
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    
+    @Test
+    public void testDeposit() 
+    {
+        Account acc = injector.getInstance(Account.class);
+        acc.deposit(500.0);
+        Assert.assertEquals(500.0, acc.getBalance(), 0);
+    }
+    
+    @Test
+    public void testWithdraw() throws Exception
+    {
+        Account acc = injector.getInstance(Account.class);
+        acc.withdraw(100.0);
+        Assert.assertEquals(-100.0, acc.getBalance(), 0);
+    }
+}
