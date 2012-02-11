d3816f9c583002f7d59a22fe3e22885b75e88105
zacblazic@gmail.com <zacblazic@gmail.com@9985715c-20a8-dc4d-9a6d-a29f60bc5474>
2012-02-11 09:04:52 +0000

    Added a sorter utility with bubble and selection sort implementations.

---
+++ Assignment1/test/za/ac/cput/sorter/test/SorterJUnitTest.java
@@ -0,0 +1,47 @@
+package za.ac.cput.sorter.test;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import za.ac.cput.sorter.Sorter;
+import za.ac.cput.sorter.conf.SorterModule;
+
+/**
+ *
+ * @author Zac Blazic
+ */
+
+public class SorterJUnitTest 
+{
+    private static Injector injector;
+    
+    public SorterJUnitTest() 
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+        injector = Guice.createInjector(new SorterModule());
+    }
+    
+    @Test
+    public void testSortAscending() 
+    {
+        Sorter sorter = injector.getInstance(Sorter.class);
+        sorter.insertArray(new int[]{5, 39, 0, -1, 100, 5});
+        
+        Assert.assertArrayEquals(new int[]{-1, 0, 5, 5, 39, 100}, sorter.sortAscending());
+    }
+    
+    @Test
+    public void testSortDescending() 
+    {
+        Sorter sorter = injector.getInstance(Sorter.class);
+        sorter.insertArray(new int[]{5, 39, 0, -1, 100, 5});
+        
+        Assert.assertArrayEquals(new int[]{-1, 0, 5, 5, 39, 100}, sorter.sortAscending());
+    }
+}
