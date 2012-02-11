9a5ebadade4c12714ae15ddaababd13dc6319ce1
zacblazic@gmail.com <zacblazic@gmail.com@9985715c-20a8-dc4d-9a6d-a29f60bc5474>
2012-02-11 17:14:41 +0000

    Missed this file.

--- Assignment1/src/za/ac/cput/database/conf/DatabaseServiceModule.java
+++ Assignment1/src/za/ac/cput/database/conf/DatabaseServiceModule.java
@@ -2,11 +2,8 @@ package za.ac.cput.database.conf;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-import java.util.HashMap;
-import java.util.Map;
 import za.ac.cput.database.DatabaseService;
 import za.ac.cput.database.internal.MockDatabaseService;
-import za.ac.cput.database.model.User;
 
 /**
  *
