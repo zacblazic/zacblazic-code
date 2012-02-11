df9d50a289afe79523e0408a6acbcc296350803a
zacblazic@gmail.com <zacblazic@gmail.com@9985715c-20a8-dc4d-9a6d-a29f60bc5474>
2012-02-11 17:12:22 +0000

    Cleaned some stuff up.

--- trunk/Assignment1/src/za/ac/cput/login/conf/LoginServiceModule.java
+++ trunk/Assignment1/src/za/ac/cput/login/conf/LoginServiceModule.java
@@ -2,11 +2,8 @@ package za.ac.cput.login.conf;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-import java.util.HashMap;
-import java.util.Map;
 import za.ac.cput.database.DatabaseService;
 import za.ac.cput.database.internal.MockDatabaseService;
-import za.ac.cput.database.model.User;
 import za.ac.cput.login.LoginService;
 import za.ac.cput.login.internal.WebLoginService;
 
@@ -20,8 +17,8 @@ public class LoginServiceModule extends AbstractModule
     @Override
     public void configure()
     {
-        bind(DatabaseService.class).to(MockDatabaseService.class);
         bind(LoginService.class).to(WebLoginService.class);
+        bind(DatabaseService.class).to(MockDatabaseService.class);
     }
     
     @Provides
