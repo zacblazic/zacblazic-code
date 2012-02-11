a603f2f12b8dbfd18e2d8443b1b9367c41a80e87
zacblazic@gmail.com <zacblazic@gmail.com@9985715c-20a8-dc4d-9a6d-a29f60bc5474>
2012-02-11 13:37:10 +0000

    Added a test suite that links all tests together for assignment 1.

---
+++ trunk/Assignment1/test/za/ac/cput/test/Assignment1TestSuite.java
@@ -0,0 +1,24 @@
+package za.ac.cput.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import za.ac.cput.test.account.AccountJUnitTest;
+import za.ac.cput.test.clock.ClockJUnitTest;
+import za.ac.cput.test.database.DatabaseServiceJUnitTest;
+import za.ac.cput.test.login.LoginServiceJUnitTest;
+import za.ac.cput.test.prime.PrimeJUnitTest;
+import za.ac.cput.test.resolver.ResolverJUnitTest;
+import za.ac.cput.test.sorter.SorterJUnitTest;
+
+/**
+ *
+ * @author Zac Blazic
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({AccountJUnitTest.class, ClockJUnitTest.class,
+        DatabaseServiceJUnitTest.class, LoginServiceJUnitTest.class, 
+        PrimeJUnitTest.class, ResolverJUnitTest.class, SorterJUnitTest.class})
+public class Assignment1TestSuite 
+{
+}
---
+++ trunk/Assignment1/test/za/ac/cput/test/login/LoginServiceJUnitTest.java
@@ -0,0 +1,64 @@
+package za.ac.cput.test.login;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import junit.framework.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import za.ac.cput.database.exception.NoSuchUserException;
+import za.ac.cput.login.LoginService;
+import za.ac.cput.login.conf.LoginServiceModule;
+import za.ac.cput.login.exception.InvalidPasswordException;
+
+/**
+ *
+ * @author Zac Blazic
+ */
+
+public class LoginServiceJUnitTest 
+{
+    private static Injector injector;
+    
+    public LoginServiceJUnitTest() 
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+        injector = Guice.createInjector(new LoginServiceModule());
+    }
+    
+    @Test
+    public void testLogin() throws Exception
+    {
+        LoginService loginService = injector.getInstance(LoginService.class);
+        loginService.login("zacblazic@gmail.com", "password");
+        
+        Assert.assertTrue(loginService.isLoggedIn("zacblazic@gmail.com"));
+    }
+    
+    @Test(expected=NoSuchUserException.class)
+    public void testLoginUsernameExeption() throws Exception
+    {
+        LoginService loginService = injector.getInstance(LoginService.class);
+        loginService.login("invalid@gmail.com", "password");
+    }
+    
+    @Test(expected=InvalidPasswordException.class)
+    public void testLoginPasswordExeption() throws Exception
+    {
+        LoginService loginService = injector.getInstance(LoginService.class);
+        loginService.login("zacblazic@gmail.com", "invalid");
+    }
+    
+    @Test
+    public void testLogout() throws Exception
+    {
+        LoginService loginService = injector.getInstance(LoginService.class);
+        loginService.login("zacblazic@gmail.com", "password");
+        loginService.logout("zacblazic@gmail.com");
+        
+        Assert.assertFalse(loginService.isLoggedIn("zacblazic@gmail.com"));
+    }
+}
