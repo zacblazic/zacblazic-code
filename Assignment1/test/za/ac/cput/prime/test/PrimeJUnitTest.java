5221066f4dbffef0f1bb9dc99fcba4319af8a759
zacblazic@gmail.com <zacblazic@gmail.com@9985715c-20a8-dc4d-9a6d-a29f60bc5474>
2012-02-11 08:12:38 +0000

    Added a utility class to generate prime numbers.

---
+++ Assignment1/test/za/ac/cput/prime/test/PrimeJUnitTest.java
@@ -0,0 +1,36 @@
+package za.ac.cput.prime.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import za.ac.cput.prime.Prime;
+
+/**
+ *
+ * @author Zac Blazic
+ */
+
+public class PrimeJUnitTest 
+{
+    public PrimeJUnitTest() 
+    {
+    }
+   
+    @Test
+    public void testIsPrime() 
+    {
+        Assert.assertTrue(Prime.isPrime(17));
+        Assert.assertTrue(Prime.isPrime(101));
+        Assert.assertTrue(Prime.isPrime(137));
+        Assert.assertFalse(Prime.isPrime(1));
+        Assert.assertFalse(Prime.isPrime(-5));
+        Assert.assertFalse(Prime.isPrime(100));
+    }
+    
+    @Test
+    public void testGeneratePrimes()
+    {
+        Assert.assertArrayEquals(new int[] {3, 5, 7}, Prime.generatePrimes(3, 3));
+        Assert.assertArrayEquals(new int[] {61, 67, 71, 73, 79}, Prime.generatePrimes(5, 60));
+        Assert.assertArrayEquals(new int[] {773, 787, 797}, Prime.generatePrimes(3, 770));
+    }
+}
