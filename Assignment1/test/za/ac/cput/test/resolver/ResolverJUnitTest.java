38e5028fc7581b886a11271bd2977e2de72a1228
zacblazic@gmail.com <zacblazic@gmail.com@9985715c-20a8-dc4d-9a6d-a29f60bc5474>
2012-02-12 06:57:23 +0000

    Added searcher unit test to test suite.

--- Assignment1/test/za/ac/cput/test/resolver/ResolverJUnitTest.java
+++ Assignment1/test/za/ac/cput/test/resolver/ResolverJUnitTest.java
@@ -1,7 +1,6 @@
 package za.ac.cput.test.resolver;
 
 import junit.framework.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import za.ac.cput.resolver.Resolver;
 
@@ -15,10 +14,10 @@ public class ResolverJUnitTest {
     {
     }
     
+    //These are bound to fail as IP addresses can change or severs can go down
     @Test(timeout=1000)
     public void testResolve() 
     {
-        //These are bound to fail as IP addresses can change
         Assert.assertEquals("198.54.223.4", Resolver.resolve("www.cput.ac.za"));
         Assert.assertEquals("208.80.152.201", Resolver.resolve("wikipedia.org"));
         Assert.assertEquals("64.34.119.12", Resolver.resolve("stackoverflow.com"));
