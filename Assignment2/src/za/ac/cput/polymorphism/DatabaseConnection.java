efed47e4e3ab39280e09e70703ab8ef9c526bcac
zacblazic@gmail.com <zacblazic@gmail.com@9985715c-20a8-dc4d-9a6d-a29f60bc5474>
2012-02-22 15:20:43 +0000

    Added a polymorphism example using connections as the theme. Also added an inheritance example displaying good and bad ways of achieving code re-use. The latter will probably be change since it does not convey the proper message, by my standards.

---
+++ Assignment2/src/za/ac/cput/polymorphism/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package za.ac.cput.polymorphism;
+
+/**
+ *
+ * @author Zac Blazic
+ */
+
+public abstract class DatabaseConnection implements Connection
+{
+    private boolean closed;
+    
+    @Override
+    public void open() 
+    {
+        closed = false;
+    }
+    
+    @Override
+    public void close() 
+    {
+        closed = true;
+    }
+    
+    @Override
+    public boolean isClosed()
+    {
+        return closed;
+    }
+}
